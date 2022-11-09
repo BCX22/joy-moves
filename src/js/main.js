2dc99865d224f9377dd36af9c4cb5f8351e679d4
Tom Stein <dev@tomstein.me>
2022-11-09 12:50:25 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
 // the link to your model provided by Teachable Machine export panel
-const URL = 'https://teachablemachine.withgoogle.com/models/smtioMGWF/';
-let model, webcam, ctx, labelContainer, maxPredictions;
+const URL = 'https://teachablemachine.withgoogle.com/models/d_xqLGdiP/';
+let model, webcam, canvas, ctx, labelContainer, maxPredictions;
 
 let lastTimeStanding;
 const totalStandingTime = 10000;
@@ -11,6 +11,7 @@ let remainingStandingTime = totalStandingTime;
 let currentlyStanding = false;
 let finished = false;
 let inOffice = false;
+let nextStepButton;
 
 function toggleOffice() {
     const toggleSwitch = document.getElementById("inOfficeToggle");
@@ -27,7 +28,11 @@ function toggleOffice() {
 
 
 async function init() {
-    document.getElementById('startbutton').remove();
+
+    nextStepButton = document.getElementById("nextStepButton");
+    nextStepButton.innerText = "Done";
+    nextStepButton.disabled = true;
+    nextStepButton.onclick = null;
 
     const modelURL = URL + 'model.json';
     const metadataURL = URL + 'metadata.json';
@@ -40,20 +45,20 @@ async function init() {
     // Convenience function to setup a webcam
     const flip = true; // whether to flip the webcam
     const exampleGuide = document.getElementById("example-guide").getBoundingClientRect();
-    webcam = new tmPose.Webcam(500, 280, flip); // width, height, flip
+    webcam = new tmPose.Webcam(1280, 720, flip); // width, height, flip
     await webcam.setup(); // request access to the webcam
     webcam.play();
     window.requestAnimationFrame(loop);
 
     // append/get elements to the DOM
-    const canvas = document.getElementById('canvas');
-    canvas.width = 500;
-    canvas.height = 280;
+    canvas = document.getElementById('canvas');
+    // canvas.width = 640;
+    // canvas.height = 360;
     ctx = canvas.getContext('2d');
-    labelContainer = document.getElementById('label-container');
-    for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement('div'));
-    }
+    // labelContainer = document.getElementById('label-container');
+    // for (let i = 0; i < maxPredictions; i++) { // and class labels
+    //     labelContainer.appendChild(document.createElement('div'));
+    // }
 
     $("#example-guide")[0].src += "?autoplay=1";
 }
@@ -71,31 +76,43 @@ async function predict() {
     // Prediction 2: run input through teachable machine classification model
     const prediction = await model.predict(posenetOutput);
 
-    for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerText = classPrediction;
+    // for (let i = 0; i < maxPredictions; i++) {
+    //     const classPrediction =
+    //         prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+    //     labelContainer.childNodes[i].innerText = classPrediction;
+    // }
+
+    const canvasSize = canvas.getBoundingClientRect();
+    ctx.drawImage(webcam.canvas, 0, 0, canvasSize.width, canvasSize.height);
+
+    // resize pose
+    if (pose) {
+        console.log(pose.keypoints)
+        for (let index = 0; index < pose.keypoints.length; index++) {
+            pose.keypoints[index].position.x = pose.keypoints[index].position.x / (1280 / canvasSize.width);
+            pose.keypoints[index].position.y = pose.keypoints[index].position.y / (720 / canvasSize.height);
+        }
+        console.log(pose.keypoints)
+
+        // finally draw the poses
+        drawPose(pose);
     }
 
-    // finally draw the poses
-    drawPose(pose);
     const standingPercentage = prediction[1].probability * 100;
     processPrediction(standingPercentage);
 }
 
 function processPrediction(standingPercentage) {
-    if (standingPercentage > 90 && !finished) {
+    if (standingPercentage > 90) {
         let now = new Date();
         if (currentlyStanding) {
             remainingStandingTime -= now - lastTimeStanding;
 
             if (remainingStandingTime > 0) {
-                document.getElementById("standing-time").innerText = "" + remainingStandingTime;
-                drawBar();
-            } else {
+                updateTime();
+            } else if (!finished) {
                 remainingStandingTime = 0;
-                document.getElementById("standing-time").innerText = "" + remainingStandingTime;
-                drawBar();
+                updateTime();
                 finish();
             }
 
@@ -104,19 +121,36 @@ function processPrediction(standingPercentage) {
         }
         lastTimeStanding = now;
         currentlyStanding = true;
-    } else if (!finished) {
+    } else {
         currentlyStanding = false;
-        updateTitle(currentlyStanding, finished);
+
+        if (!finished) {
+            updateTitle(currentlyStanding, finished);
+        }
     }
+    updateState(currentlyStanding);
 }
 
 function finish() {
     finished = true;
     updateTitle(false, finished);
 
-    const doneBtn = document.createElement("button");
-    doneBtn.innerText = "Done"
-    document.getElementById("button-div").appendChild(doneBtn);
+    nextStepButton.disabled = false;
+    nextStepButton.onclick = onDoneClick();
+}
+
+function onDoneClick() {
+
+}
+
+function updateState(standing) {
+    const stateElement = document.getElementById("standing-state");
+
+    if (standing) {
+        stateElement.innerText = "Standing"
+    } else {
+        stateElement.innerText = "Sitting"
+    }
 }
 
 function updateTitle(standing, finished) {
@@ -128,20 +162,21 @@ function updateTitle(standing, finished) {
     else if (standing) {
         titleElement.innerText = "Just Keep moving 💃"
     } else {
-        titleElement.innerText = "Get up for joy moves!"
+        titleElement.innerText = "Get up for your joy moves!"
     }
 }
 
-function drawBar() {
-    const percentage = remainingStandingTime * 100 / totalStandingTime;
-    const bar = document.getElementById("standing-time-bar");
-    var color1 = "#797979";
-    var color2 = "#D4D4D4";
-    bar.style.background = `linear-gradient(to right, ${color1}, ${percentage}%, ${color2})`;
+function updateTime() {
+    // const percentage = remainingStandingTime * 100 / totalStandingTime;
+    // const bar = document.getElementById("standing-time-bar");
+    // var color1 = "#797979";
+    // var color2 = "#D4D4D4";
+    // bar.style.background = `linear-gradient(to right, ${color1}, ${percentage}%, ${color2})`;
+
+    document.getElementById("standing-timer").innerText = "Timer: " + (remainingStandingTime / 1000).toFixed(2) + "s"
 }
 
 function drawPose(pose) {
-    ctx.drawImage(webcam.canvas, 0, 0);
     // draw the keypoints and skeleton
     if (pose) {
         const minPartConfidence = 0.5;
@@ -149,3 +184,7 @@ function drawPose(pose) {
         tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
     }
 }
+
+$(document).ready(function () {
+    updateTime();
+});
